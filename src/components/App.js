60046a259888d23bed5822b96230aa0687f62350
JaniceNova <janicenovacek2016@gmail.com>
2019-01-05 17:54:09 -0800

    got app to show up on screen

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React from'react';
 
-const App =() => {
-    return <div>App</div>;
+const App = () => {
+    return( 
+    <div>Apfdsap</div>
+    );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from'react';
 import ReactDOM from 'react-dom';
-
-
 import App from './components/App';
-ReactDOM.render(<app />, document.querySelect=('#root'));
+
+
+ReactDOM.render(<App />, document.querySelector('#root'));
